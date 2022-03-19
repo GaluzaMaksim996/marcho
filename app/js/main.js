8d73c90d05e175691f88d346080b9acfd047356c
Maksim <galuza.maksim996@gmail.com>
2022-03-19 22:52:48 +0600

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
-$(function() {
+$(function () {
+
+  $('.shop__filter-btn').on('click', function () {
+    $('.shop__filters').slideToggle()
+  })
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+  })
 
   $('.blog-page__slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g id="surface1"><path d="M 5.25 12.25 C 5.027344 12.25 4.800781 12.164062 4.632812 11.992188 L 0.257812 7.617188 C -0.0859375 7.277344 -0.0859375 6.722656 0.257812 6.382812 L 4.632812 2.007812 C 4.972656 1.664062 5.527344 1.664062 5.867188 2.007812 C 6.210938 2.347656 6.210938 2.902344 5.867188 3.242188 L 2.113281 7 L 5.871094 10.757812 C 6.210938 11.097656 6.210938 11.652344 5.871094 11.996094 C 5.699219 12.164062 5.472656 12.25 5.25 12.25 Z M 5.25 12.25 "/></g></svg></button>',
@@ -6,7 +14,7 @@ $(function() {
     infinite: false,
   })
 
-  $('.product-tabs__top-item').on('click', function(e) {
+  $('.product-tabs__top-item').on('click', function (e) {
     e.preventDefault()
     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active')
     $(this).addClass('product-tabs__top-item--active')
@@ -32,19 +40,29 @@ $(function() {
     draggable: false,
     arrows: false,
     fade: true,
+    responsive: [
+      {
+        breakpoint: 1051,
+        settings: {
+          draggable: true,
+        }
+      }
+    ],
   })
 
-  $('.shop-content__filter-btn').on('click', function() {
+  $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
     $(this).addClass('shop-content__filter-btn--active')
   })
 
-  $('.button-list').on('click', function() {
+  $('.button-list').on('click', function () {
     $('.product-item').addClass('product-item--list')
+    $('.shop-content__inner').addClass('shop-content__nogrid')
   })
 
-  $('.button-grid').on('click', function() {
+  $('.button-grid').on('click', function () {
     $('.product-item').removeClass('product-item--list')
+    $('.shop-content__inner').removeClass('shop-content__nogrid')
   })
 
   $('.select-style, .product-one__num').styler()
@@ -84,7 +102,7 @@ $(function() {
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -93,31 +111,31 @@ $(function() {
       seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
     const clock = document.querySelector('.promo__clock');
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
-  
+
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
 })
